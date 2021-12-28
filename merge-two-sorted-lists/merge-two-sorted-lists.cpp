bbd124db10009853932592965ea0728912855680
Shweta2762 <89833154+Shweta2762@users.noreply.github.com>
2021-12-28 23:43:59 +0530

    Time: 8 ms (68.90%), Space: 14.8 MB (84.26%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,62 +11,11 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        /*vector<int>ree;
-        //ListNode* list1ree=list1;
-        while(list1!=NULL)
-            ree.push_back(list1->val);
-        //ListNode *list2ree=list2;
-        while(list2!=NULL)
-            ree.push_back(list2->val);
-        sort(ree.begin(),ree.end());
-        ListNode* mergedl;
-        for(int i=0;i<ree.size();i++)
+        ListNode *currptr= new ListNode(0);    // New node created
+        ListNode *curr= currptr;          //  defining pointer on the new node.
+        while(list1!=NULL && list2!=NULL)
         {
-            mergedl->val=ree[i];
-            mergedl=mergedl->next;
-        }
-        mergedl->next=NULL;
-        return mergedl;*/
-            //ListNode *currptr= new ListNode(0);    // New node created
-            //ListNode *curr= currptr;          //  defining pointer on the new node.
-
-        //curr=NULL;
-        ListNode *curr= new ListNode(0);    // New node created
-	
-    ListNode *currptr= curr;          //  defining pointer on the new node.
-	
-	// Case1: where both linkedlist is non-empty
-    while(list1 !=NULL and list2 !=NULL){
-        if (list1->val <= list2->val){
-            currptr->next=list1;
-            list1=list1->next;
-        }
-        else{
-            currptr->next=list2;
-            list2=list2->next;
-        }   
-        currptr=currptr->next;
-    }
-	
-	// Case2: Where list 2 is now empty but list1 has element so join list1 elements with curr node list.
-    while(list1 !=NULL){
-        currptr->next=list1;
-        list1=list1->next;
-        currptr=currptr->next;
-    }
-	
-	// Case2: Where list1  is now empty but list2 has element so join list2 elements with curr node list.
-    while(list2 !=NULL){
-        currptr->next=list2;
-        list2=list2->next;
-        currptr=currptr->next;
-    }
-	
-	// Returning new node next pointer as the pointer created on curr node has now reached at last of the new formed sorted combined list.
-    return curr->next;
-        /*while(list1!=NULL && list2!=NULL)
-        {
-            if(list1->val>list2->val)
+            if(list1->val<list2->val)
             {
                 curr->next=list1;
                 list1=list1->next;
@@ -90,7 +39,7 @@ public:
             list2=list2->next;
             curr=curr->next;
         }
-        return currptr->next;*/
+        return currptr->next;
             
     }
 };
