4a6d64ca5fe8dba214b0bca26a3dfa6e93a3f010
Shweta2762 <89833154+Shweta2762@users.noreply.github.com>
2022-06-04 11:36:31 +0530

    Time: 4 ms (82.31%), Space: 16.4 MB (78.14%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -36,9 +36,11 @@ public:
 //         return isMirrorHelp(root,mirror);
 //     }
      bool check(TreeNode* root1,TreeNode* root2){
-        if(root1==nullptr or root2==nullptr){
-            return root1==root2;
+        if(root1==nullptr && root2==nullptr){
+            return true;
         }
+        else if(root1==nullptr || root2==nullptr)
+            return false;
         return root1->val==root2->val and check(root1->left,root2->right) and check(root1->right,root2->left);
     }
     bool isSymmetric(TreeNode* root) {
