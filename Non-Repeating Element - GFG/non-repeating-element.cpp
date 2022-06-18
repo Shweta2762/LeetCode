7e00eda10f4cdf7e1981822ce4cdcd1392319487
Shweta2762 <89833154+Shweta2762@users.noreply.github.com>
2022-06-18 23:20:47 +0530

    Added solution - LeetHub

--- Non-Repeating Element - GFG/non-repeating-element.cpp
+++ Non-Repeating Element - GFG/non-repeating-element.cpp
@@ -10,6 +10,20 @@ class Solution{
     public:
     int firstNonRepeating(int arr[], int n) 
     {
+        // unordered_map<int,int> umap;
+        // for(int i=0;i<n;i++)
+        // {
+        //     int key=arr[i];
+        //     umap[key]++;
+        // }
+        // for(int i=0;i<n;i++)
+        // {
+        //     int key=arr[i];
+        //     auto temp=umap.find(key);
+        //     if(temp->second==1)
+        //     return key;
+        // }
+        // // Complete the function
         unordered_map<int,int> umap;
         for(int i=0;i<n;i++)
         {
@@ -23,8 +37,7 @@ class Solution{
             if(temp->second==1)
             return key;
         }
-        // Complete the function
-        
+        return -1;
     } 
   
 };
