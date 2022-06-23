ceba62c147f91f3e9a1374097a2a00e633997bf6
Shweta2762 <89833154+Shweta2762@users.noreply.github.com>
2022-06-23 18:04:39 +0530

    Time: 40 ms (80.41%), Space: 71.4 MB (80.83%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -44,7 +44,7 @@ public:
         ListNode* dummy=new ListNode();
         ListNode* temp=dummy;
         int carry=0;
-        while(l1!=NULL || l2!=NULL || carry!=0)
+        while(l1!=NULL||l2!=NULL||carry!=0)
         {
             int sum=0;
             if(l1!=NULL)
@@ -65,5 +65,30 @@ public:
             
         }
         return dummy->next;
+
+//             ListNode* dummy=new ListNode();
+//         ListNode* temp=dummy;
+//         int carry=0;
+//         while(l1!=NULL || l2!=NULL || carry!=0)
+//         {
+//             int sum=0;
+//             if(l1!=NULL)
+//             {
+//                 sum=sum+l1->val;
+//                 l1=l1->next;
+//             }
+//             if(l2!=NULL)
+//             {
+//                 sum=sum+l2->val;
+//                 l2=l2->next;
+//             }
+//             sum=sum+carry;
+//             carry=sum/10;
+//             ListNode *node=new ListNode(sum%10);
+//             temp->next=node;
+//             temp=temp->next;
+            
+//         }
+//         return dummy->next;
     }
 };
